5dc835cd97e8fb8f2ba7340b4822598869c34a35
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-11-12 08:31:54 +0100

    add pti colors

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import { colors } from './theme';
 
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: 'light',
+  },
+  colors: colors,
 });
 
 createApp(App).use(router).use(vuetify).mount('#app');
