d172e0b224cae47645504bce9c003638c83fc8c0
Noud Goedemondt <noudgoedemondt@gmail.com>
2024-11-13 20:46:31 +0100

    add v-menu nav items

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -6,63 +6,44 @@
           <v-img :src="logo" width="120"></v-img>
         </v-app-bar-title>
 
-        <div class="d-flex align-center">
-          <v-btn
-            v-for="(menuItem, index) in menuItems"
-            :key="index"
-            class="mx-2"
-            variant="text"
-            @mouseenter="openMenu(index)"
-            @mouseleave="closeMenu"
-          >
-            {{ menuItem.title }}
-            <v-icon right>
-              {{ activeMenu === index ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
-            </v-icon>
-          </v-btn>
-        </div>
+        <v-menu
+          v-for="(menuItem, index) in menuItems"
+          :key="index"
+          v-model="activeMenu"
+        >
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" class="mx-2" variant="text">
+              {{ menuItem.title }}
+            </v-btn>
+          </template>
+
+          <v-list v-if="activeMenu === index">
+            <v-list-item
+              v-for="(item, itemIndex) in menuItem.items"
+              :key="itemIndex"
+              :to="item.route"
+            >
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </v-container>
     </v-app-bar>
-
-    <!-- Expanding menu panel -->
-    <v-expand-transition>
-      <v-sheet
-        v-if="activeMenu !== null"
-        class="position-absolute left-0 right-0"
-        style="top: 80px; z-index: 10"
-        @mouseenter="keepMenuOpen"
-        @mouseleave="closeMenu"
-      >
-        <v-list class="pa-4">
-          <v-row>
-            <v-col
-              v-for="(item, index) in activeMenuItems"
-              :key="index"
-              cols="3"
-            >
-              <v-list-item :to="item.route" link>
-                <template v-slot:prepend>
-                  <v-icon>{{ item.icon }}</v-icon>
-                </template>
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item>
-            </v-col>
-          </v-row>
-        </v-list>
-      </v-sheet>
-    </v-expand-transition>
   </div>
 </template>
 
 <script setup>
-//TODO: menu items zijn stuk, maak bestaande routes
-//TODO: menu item lay out is stuk
+/*
+ * TODO
+ * --navigatie menu items fixen
+ * --menu item layout veranderen
+ */
 
-import { ref, computed } from 'vue';
+import { ref } from 'vue';
 import logo from '@/assets/PTI-logo_landscape.svg';
 
 const activeMenu = ref(null);
-const closeTimeout = ref(null);
+// const closeTimeout = ref(null);
 const menuItems = ref([
   {
     title: 'Branches',
@@ -97,17 +78,17 @@ const menuItems = ref([
   },
 ]);
 
-const activeMenuItems = computed(() =>
-  activeMenu.value !== null ? menuItems.value[activeMenu.value].items : []
-);
-const openMenu = (index) => {
-  clearTimeout(closeTimeout.value);
-  activeMenu.value = index;
-};
-const closeMenu = () => {
-  closeTimeout.value = setTimeout(() => {
-    activeMenu.value = null;
-  }, 150);
-};
-const keepMenuOpen = () => clearTimeout(closeTimeout.value);
+// const activeMenuItems = computed(() =>
+//   activeMenu.value !== null ? menuItems.value[activeMenu.value].items : []
+// );
+// const openMenu = (index) => {
+//   clearTimeout(closeTimeout.value);
+//   activeMenu.value = index;
+// };
+// const closeMenu = () => {
+//   closeTimeout.value = setTimeout(() => {
+//     activeMenu.value = null;
+//   }, 150);
+// };
+// const keepMenuOpen = () => clearTimeout(closeTimeout.value);
 </script>
