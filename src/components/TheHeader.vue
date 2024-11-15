f7d0352498f03a21cd74ecd8cd45e5b9a95f14e4
Noud Goedemondt <noudgoedemondt@gmail.com>
2024-11-15 13:54:12 +0100

    changed app bar nav drawer

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,44 +1,49 @@
 <template>
   <div>
-    <v-expand-transition>
-      <v-app-bar :height="activeMenuItemIndex !== null ? 280 : 64">
-        <v-container max-width="70vw" class="top-container d-flex align-center">
-          <v-app-bar-title>
-            <v-img :src="logo" width="120"></v-img>
-          </v-app-bar-title>
+    <v-app-bar :height="activeMenuItemIndex !== null ? 280 : 64">
+      <v-container max-width="70vw" class="top-container d-flex align-center">
+        <v-app-bar-title>
+          <router-link to="/">
+            <v-img :src="logo" width="120" class="cursos-pointer"></v-img>
+          </router-link>
+        </v-app-bar-title>
 
-          <v-btn
-            v-for="(items, index) in menuItems"
-            :key="index"
-            @click="setActiveMenuItemIndex(index)"
-          >
-            {{ items.title }}
-          </v-btn>
-        </v-container>
-
-        <v-container
-          v-if="activeMenuItemIndex !== null"
-          max-width="70vw"
-          class="expanded-content"
+        <v-btn
+          v-for="(items, index) in menuItems"
+          :key="index"
+          @click="setActiveMenuItemIndex(index)"
+          :prepend-icon="
+            index === activeMenuItemIndex
+              ? 'mdi-chevron-down'
+              : 'mdi-chevron-up'
+          "
         >
-          <v-row>
-            <v-col
-              cols="12"
-              md="4"
-              v-for="(subItems, index) in activeMenuItems"
-              :key="index"
-            >
-              <v-list-item :to="subItems.route" class="px-2">
-                <template v-slot:prepend>
-                  <v-icon :icon="subItems.icon"></v-icon>
-                </template>
-                <v-list-item-title>{{ subItems.title }}</v-list-item-title>
-              </v-list-item>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-app-bar>
-    </v-expand-transition>
+          {{ items.title }}
+        </v-btn>
+      </v-container>
+
+      <v-container
+        v-if="activeMenuItemIndex !== null"
+        max-width="70vw"
+        class="expanded-content"
+      >
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+            v-for="(subItems, index) in activeMenuItems"
+            :key="index"
+          >
+            <v-list-item :to="subItems.route">
+              <template v-slot:prepend>
+                <v-icon :icon="subItems.icon"></v-icon>
+              </template>
+              <v-list-item-title>{{ subItems.title }}</v-list-item-title>
+            </v-list-item>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
   </div>
 </template>
 
@@ -52,32 +57,40 @@ const menuItems = ref([
   {
     title: 'Branches',
     items: [
-      { title: 'Bouw en Vastgoed', icon: 'mdi-view-grid', route: '/' },
-      { title: 'IT en ITES', icon: 'mdi-shape', route: '/' },
-      { title: 'Gezondheidszorg', icon: 'mdi-new-box', route: '/' },
-      { title: 'Productie', icon: 'mdi-tag', route: '/' },
-      { title: 'Vervoer en Logistiek', icon: 'mdi-view-grid', route: '/' },
+      { title: 'Bouw en Vastgoed', icon: 'mdi-home-city', route: '/test' },
+      { title: 'IT en ITES', icon: 'mdi-desktop-classic', route: '/test' },
+      { title: 'Gezondheidszorg', icon: 'mdi-hospital-box', route: '/test' },
+      { title: 'Productie', icon: 'mdi-factory', route: '/test' },
+      { title: 'Vervoer en Logistiek', icon: 'mdi-truck-fast', route: '/test' },
     ],
   },
   {
     title: 'Modules',
     items: [
-      { title: 'Werkorderbeheer', icon: 'mdi-view-grid', route: '/' },
-      { title: 'Gebruikersbeheer', icon: 'mdi-shape', route: '/' },
-      { title: 'Procesbeheer', icon: 'mdi-view-grid', route: '/' },
-      { title: 'Projectbeheer', icon: 'mdi-tag', route: '/' },
-      { title: 'Planning', icon: 'mdi-new-box', route: '/' },
-      { title: 'Verdeeltool', icon: 'mdi-view-grid', route: '/' },
-      { title: 'Klantenportaal', icon: 'mdi-view-grid', route: '/' },
-      { title: 'Klantcommunicatie', icon: 'mdi-view-grid', route: '/' },
+      {
+        title: 'Werkorderbeheer',
+        icon: 'mdi-file-document-outline',
+        route: '/test',
+      },
+      { title: 'Gebruikersbeheer', icon: 'mdi-account-group', route: '/test' },
+      { title: 'Procesbeheer', icon: 'mdi-cogs', route: '/test' },
+      { title: 'Projectbeheer', icon: 'mdi-briefcase-outline', route: '/test' },
+      { title: 'Planning', icon: 'mdi-calendar-clock', route: '/test' },
+      { title: 'Verdeeltool', icon: 'mdi-arrow-decision', route: '/test' },
+      {
+        title: 'Klantenportaal',
+        icon: 'mdi-account-arrow-right',
+        route: '/test',
+      },
+      { title: 'Klantcommunicatie', icon: 'mdi-email-outline', route: '/test' },
     ],
   },
   {
     title: 'Tarieven',
     items: [
-      { title: 'Calculator', icon: 'mdi-view-grid', route: '/' },
-      { title: 'Lorem', icon: 'mdi-shape', route: '/' },
-      { title: 'Lorem', icon: 'mdi-new-box', route: '/' },
+      { title: 'Calculator', icon: 'mdi-calculator', route: '/test' },
+      { title: 'Lorem', icon: 'mdi-information-outline', route: '/test' },
+      { title: 'Lorem', icon: 'mdi-file-outline', route: '/test' },
     ],
   },
 ]);
