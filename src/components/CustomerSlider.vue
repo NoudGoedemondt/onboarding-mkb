8a458369e5d97fd8b8acf8b688b547d4a4ef1435
Noud Goedemondt <noudgoedemondt@gmail.com>
2024-11-18 15:29:40 +0100

    add customer slider

---
+++ src/components/CustomerSlider.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card class="mx-auto mt-10" max-width="70vw" flat>
+    <v-card-title class="text-center text-h4">Onze Klanten</v-card-title>
+
+    <v-slide-group show-arrows="always" :key="customerImages.length">
+      <v-slide-group-item
+        v-for="(image, index) in customerImages"
+        :key="index"
+        class="d-flex align-center justify-center"
+      >
+        <v-img
+          :src="image"
+          width="200px"
+          height="150px"
+          contain
+          class="mx-5"
+        ></v-img>
+      </v-slide-group-item>
+    </v-slide-group>
+  </v-card>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const customerImages = ref([]);
+
+onMounted(() => {
+  const images = require.context('@/assets/customers', false, /\.png$/);
+  customerImages.value = images.keys().map((key) => images(key));
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <the-hero />
+  <customer-slider />
 </template>
 
 <script setup>
 import TheHero from '@/components/TheHero.vue';
+import CustomerSlider from '@/components/CustomerSlider.vue';
 </script>
