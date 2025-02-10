f0e77dc2fc287a79e5e1ee46ec1b54a42406e313
NoudGoedemondt <noudgoedemondt@gmail.com>
2025-02-10 09:54:05 +0100

    fix routes, add testview

--- src/router.js
+++ src/router.js
@@ -1,17 +1,19 @@
-import { createRouter, createMemoryHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import HomeView from './views/HomeView.vue';
 import ModuleView from './views/ModuleView.vue';
 import BrancheView from './views/BrancheView.vue';
+import TestView from './views/TestView.vue';
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/modules', component: ModuleView },
   { path: '/branches', component: BrancheView },
+  { path: '/test', component: TestView },
 ];
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 });
 
